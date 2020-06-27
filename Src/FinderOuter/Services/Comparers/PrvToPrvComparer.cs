5995eb0a881c6370c0e14fb0a1bbaa6af20dddab
Coding Enthusiast <CodingEnthusiast@protonmail.com>
2020-06-27 12:08:54 +0430

    Add a new comparer

---
+++ Src/FinderOuter/Services/Comparers/PrvToPrvComparer.cs
@@ -0,0 +1,34 @@
+﻿// The FinderOuter
+// Copyright (c) 2020 Coding Enthusiast
+// Distributed under the MIT software license, see the accompanying
+// file LICENCE or http://www.opensource.org/licenses/mit-license.php.
+
+using Autarkysoft.Bitcoin.Cryptography.Asymmetric.KeyPairs;
+using System;
+
+namespace FinderOuter.Services.Comparers
+{
+    /// <summary>
+    /// Compares 2 private key bytes. It is useful for HD keys where user has a single child private key.
+    /// </summary>
+    public class PrvToPrvComparer : ICompareService
+    {
+        private byte[] expected;
+
+        public bool Init(string data)
+        {
+            try
+            {
+                using PrivateKey temp = new PrivateKey(data);
+                expected = temp.ToBytes();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public bool Compare(byte[] key) => ((ReadOnlySpan<byte>)expected).SequenceEqual(key);
+    }
+}
---
+++ Src/Tests/Services/Comparers/PrvToPrvComparerTests.cs
@@ -0,0 +1,47 @@
+﻿// The FinderOuter
+// Copyright (c) 2020 Coding Enthusiast
+// Distributed under the MIT software license, see the accompanying
+// file LICENCE or http://www.opensource.org/licenses/mit-license.php.
+
+using FinderOuter.Services.Comparers;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Tests.Services.Comparers
+{
+    public class PrvToPrvComparerTests
+    {
+        public static IEnumerable<object[]> GetHashCases()
+        {
+            yield return new object[] { KeyHelper.Prv1.ToWif(true), true };
+            yield return new object[] { KeyHelper.Prv1.ToWif(false), true };
+            yield return new object[] { KeyHelper.Prv1.ToWif(false) + "1", false };
+            yield return new object[] { "FOO", false };
+        }
+
+        [Theory]
+        [MemberData(nameof(GetHashCases))]
+        public void InitTest(string wif, bool expected)
+        {
+            var comp = new PrvToPrvComparer();
+            bool actual = comp.Init(wif);
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void CompareTest()
+        {
+            var comp = new PrvToPrvComparer();
+            Assert.True(comp.Init(KeyHelper.Prv1.ToWif(true)));
+            byte[] key = KeyHelper.Prv1.ToBytes();
+            key[0]++;
+
+            bool b = comp.Compare(key);
+            Assert.False(b);
+
+            key[0]--;
+            b = comp.Compare(key);
+            Assert.True(b);
+        }
+    }
+}
