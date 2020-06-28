de9bc1ab8074853df395069c04b1dd980e21761b
Coding Enthusiast <CodingEnthusiast@protonmail.com>
2020-06-28 07:59:36 +0430

    Add a new model to be used by UI elements that need description using enums.

---
+++ Src/FinderOuter/Models/DescriptiveItem.cs
@@ -0,0 +1,28 @@
+﻿// The FinderOuter
+// Copyright (c) 2020 Coding Enthusiast
+// Distributed under the MIT software license, see the accompanying
+// file LICENCE or http://www.opensource.org/licenses/mit-license.php.
+
+using System;
+using System.ComponentModel;
+using System.Reflection;
+
+namespace FinderOuter.Models
+{
+    public class DescriptiveItem<T> where T : Enum
+    {
+        public DescriptiveItem(T value)
+        {
+            Value = value;
+
+            FieldInfo fi = value.GetType().GetField(value.ToString());
+            object[] attributes = fi?.GetCustomAttributes(typeof(DescriptionAttribute), false);
+            Description = (attributes != null && attributes.Length != 0) ?
+                                                                ((DescriptionAttribute)attributes[0]).Description :
+                                                                value.ToString();
+        }
+
+        public string Description { get; set; }
+        public T Value { get; set; }
+    }
+}
---
+++ Src/Tests/Models/DescriptiveItemTests.cs
@@ -0,0 +1,37 @@
+﻿// The FinderOuter
+// Copyright (c) 2020 Coding Enthusiast
+// Distributed under the MIT software license, see the accompanying
+// file LICENCE or http://www.opensource.org/licenses/mit-license.php.
+
+using FinderOuter.Models;
+using System.ComponentModel;
+using Xunit;
+
+namespace Tests.Models
+{
+    public class DescriptiveItemTests
+    {
+        public enum MockEnum
+        {
+            [Description("Foo desc.")]
+            Foo,
+            [Description("Bar desc.")]
+            Bar,
+            // No desc.
+            FooBar
+        }
+
+        [Theory]
+        [InlineData(MockEnum.Foo, "Foo desc.")]
+        [InlineData(MockEnum.Bar, "Bar desc.")]
+        [InlineData(MockEnum.FooBar, "FooBar")]
+        [InlineData((MockEnum)123, "123")]
+        public void ConstructorTest(MockEnum val, string expected)
+        {
+            var item = new DescriptiveItem<MockEnum>(val);
+
+            Assert.Equal(expected, item.Description);
+            Assert.Equal(val, item.Value);
+        }
+    }
+}
