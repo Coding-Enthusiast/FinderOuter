cc87bc702d7915c73d5f8f0292e7e46c0977a474
Coding Enthusiast <CodingEnthusiast@protonmail.com>
2020-06-29 09:27:41 +0430

    Add some MnemonicSevice tests

---
+++ Src/Tests/Services/MnemonicSeviceTests.cs
@@ -0,0 +1,78 @@
+﻿// The FinderOuter
+// Copyright (c) 2020 Coding Enthusiast
+// Distributed under the MIT software license, see the accompanying
+// file LICENCE or http://www.opensource.org/licenses/mit-license.php.
+
+using Autarkysoft.Bitcoin;
+using Autarkysoft.Bitcoin.ImprovementProposals;
+using FinderOuter.Services;
+using System;
+using System.Text;
+using Xunit;
+
+namespace Tests.Services
+{
+    public class MnemonicSeviceTests
+    {
+        [Fact]
+        public void SpaceByteCostTest()
+        {
+            byte[] expected = Encoding.UTF8.GetBytes(" ");
+            Assert.Single(expected);
+            Assert.Equal(expected[0], MnemonicSevice.SpaceByte);
+        }
+
+        [Fact]
+        public void TrySetWordListTest()
+        {
+            var serv = new MnemonicSevice(null);
+            Array wls = Enum.GetValues(typeof(BIP0039.WordLists));
+            byte[] space = Encoding.UTF8.GetBytes(" ");
+            Assert.Single(space);
+
+            foreach (BIP0039.WordLists item in wls)
+            {
+                bool b = serv.TrySetWordList(item, out string[] words, out int actualMaxWordLen);
+                Assert.True(b);
+                Assert.Equal(2048, words.Length);
+
+                string bigSeed = string.Join(" ", words);
+                int expectedMaxWordLen = 0;
+                FastStream stream = new FastStream(37831);
+                for (int i = 0; i < words.Length; i++)
+                {
+                    byte[] wordBa = Encoding.UTF8.GetBytes(words[i]);
+                    stream.Write(wordBa);
+                    if (expectedMaxWordLen < wordBa.Length)
+                    {
+                        expectedMaxWordLen = wordBa.Length;
+                    }
+                    if (i != 2047)
+                    {
+                        stream.Write(space);
+                    }
+                }
+
+                byte[] actual = stream.ToByteArray();
+                byte[] expected = Encoding.UTF8.GetBytes(bigSeed);
+
+                Assert.Equal(expected, actual);
+                Assert.Equal(expectedMaxWordLen, actualMaxWordLen);
+            }
+        }
+
+        [Fact]
+        public void GetSeedByteTest()
+        {
+            var serv = new MnemonicSevice(null);
+            byte[] actual = serv.GetSeedByte(12, 3);
+            Assert.Equal(47, actual.Length);
+
+            actual = serv.GetSeedByte(12, 8);
+            Assert.Equal(107, actual.Length);
+
+            actual = serv.GetSeedByte(24, 33);
+            Assert.Equal(815, actual.Length);
+        }
+    }
+}
