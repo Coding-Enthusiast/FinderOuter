059c70901e5b9c98542c381d47af4944645c5e76
Coding Enthusiast <CodingEnthusiast@protonmail.com>
2020-12-23 15:46:22 +0330

    Add a helper struct to copy 32 byte blocks

--- Src/FinderOuter/BlockCopyHelpers.cs
+++ Src/FinderOuter/BlockCopyHelpers.cs
@@ -7,6 +7,9 @@ using System.Runtime.InteropServices;
 
 namespace FinderOuter
 {
+    [StructLayout(LayoutKind.Sequential, Size = 32)]
+    internal struct Block32 { }
+
     [StructLayout(LayoutKind.Sequential, Size = 64)]
     internal struct Block64 { }
 }
