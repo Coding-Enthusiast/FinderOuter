74dc069a70ed8b509eee6f627e6ed9b9b290d4eb
Coding Enthusiast <CodingEnthusiast@protonmail.com>
2021-05-12 08:36:13 +0430

    Add RIPEMD160 tests

---
+++ Src/Tests/Backend/Cryptography/Hashing/Ripemd160Tests.cs
@@ -0,0 +1,80 @@
+﻿// The FinderOuter
+// Copyright (c) 2020 Coding Enthusiast
+// Distributed under the MIT software license, see the accompanying
+// file LICENCE or http://www.opensource.org/licenses/mit-license.php.
+
+using FinderOuter.Backend.Cryptography.Hashing;
+using Newtonsoft.Json.Linq;
+using System.Text;
+using Xunit;
+
+namespace Tests.Backend.Cryptography.Hashing
+{
+    public class Ripemd160Tests
+    {
+        [Theory]
+        [MemberData(nameof(HashTestCaseHelper.GetRegularHashCases), parameters: "RIPEMD160", MemberType = typeof(HashTestCaseHelper))]
+        public void ComputeHashTest(byte[] message, byte[] expectedHash)
+        {
+            byte[] actualHash = Ripemd160Fo.ComputeHash_Static(message);
+            Assert.Equal(expectedHash, actualHash);
+        }
+
+        private static byte[] GetBytes(int len)
+        {
+            byte[] result = new byte[len];
+            for (int i = 0; i < len; i++)
+            {
+                result[i] = (byte)(i + 1);
+            }
+
+            return result;
+        }
+        public static TheoryData GetProgressiveCase()
+        {
+            var result = new TheoryData<byte[], byte[]>();
+            int len = 1;
+            // Hash values were computed using .Net framework 4.7.2 System.Security.Cryptography.RIPEMD160Managed
+            foreach (var item in Helper.ReadResources<JArray>("Ripemd160ProgressiveTestData"))
+            {
+                byte[] msgBytes = GetBytes(len++);
+                byte[] hashBytes = Helper.HexToBytes(item.ToString());
+
+                result.Add(msgBytes, hashBytes);
+            }
+            return result;
+        }
+        [Theory]
+        [MemberData(nameof(GetProgressiveCase))]
+        public void ComputeHash_ProgressiveTest(byte[] message, byte[] expectedHash)
+        {
+            byte[] actualHash = Ripemd160Fo.ComputeHash_Static(message);
+            Assert.Equal(expectedHash, actualHash);
+        }
+
+        [Fact]
+        public void ComputeHash_AMillionATest()
+        {
+            byte[] actualHash = Ripemd160Fo.ComputeHash_Static(HashTestCaseHelper.GetAMillionA());
+            byte[] expectedHash = Helper.HexToBytes("52783243c1697bdbe16d37f97f68f08325dc1528");
+
+            Assert.Equal(expectedHash, actualHash);
+        }
+
+        [Fact]
+        public void ComputeHash_ReuseTest()
+        {
+            // From https://en.wikipedia.org/wiki/RIPEMD#RIPEMD-160_hashes
+            byte[] msg1 = Encoding.UTF8.GetBytes("The quick brown fox jumps over the lazy dog");
+            byte[] msg2 = Encoding.UTF8.GetBytes("The quick brown fox jumps over the lazy cog");
+            byte[] exp1 = Helper.HexToBytes("37f332f68db77bd9d7edd4969571ad671cf9dd3b");
+            byte[] exp2 = Helper.HexToBytes("132072df690933835eb8b6ad0b77e7b6f14acad7");
+
+            byte[] act1 = Ripemd160Fo.ComputeHash_Static(msg1);
+            byte[] act2 = Ripemd160Fo.ComputeHash_Static(msg2);
+
+            Assert.Equal(exp1, act1);
+            Assert.Equal(exp2, act2);
+        }
+    }
+}
