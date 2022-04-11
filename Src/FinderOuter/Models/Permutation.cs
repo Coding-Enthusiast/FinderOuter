ddfff5fdceaec57f42ea9af9e3759e603947a1c6
Coding Enthusiast <CodingEnthusiast@protonmail.com>
2022-04-11 11:51:54 +0430

    Add permutation object

---
+++ Src/FinderOuter/Models/Permutation.cs
@@ -0,0 +1,47 @@
+﻿// The FinderOuter
+// Copyright (c) 2020 Coding Enthusiast
+// Distributed under the MIT software license, see the accompanying
+// file LICENCE or http://www.opensource.org/licenses/mit-license.php.
+
+using System.Diagnostics;
+
+namespace FinderOuter.Models
+{
+    public unsafe struct Permutation
+    {
+        public Permutation(int maximum, uint* values)
+        {
+            max = maximum;
+            pt = values;
+            index = 0;
+        }
+
+
+        public readonly int max;
+        private int index;
+        private readonly uint* pt;
+        
+
+        public uint GetValue() => pt[index];
+
+        public uint GetNextValue()
+        {
+            Debug.Assert(index + 1 < max);
+            return pt[index++];
+        }
+
+        public bool Increment()
+        {
+            index++;
+            if (index == max)
+            {
+                index = 0;
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
