fafaa8c7434081c8c56ca451d6ec875a7bd2fee6
Coding Enthusiast <CodingEnthusiast@protonmail.com>
2022-04-27 12:52:23 +0430

    Add a new close event to VMs

--- Src/FinderOuter/ViewModels/ViewModelBase.cs
+++ Src/FinderOuter/ViewModels/ViewModelBase.cs
@@ -4,10 +4,14 @@
 // file LICENCE or http://www.opensource.org/licenses/mit-license.php.
 
 using ReactiveUI;
+using System;
 
 namespace FinderOuter.ViewModels
 {
     public class ViewModelBase : ReactiveObject
     {
+        public event EventHandler CLoseEvent;
+
+        public void RaiseCloseEvent() => CLoseEvent?.Invoke(this, null);
     }
 }
