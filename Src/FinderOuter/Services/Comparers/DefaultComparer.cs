d048c4ce5ecb51a28c85a6ca019c05bf46c72e1d
Coding Enthusiast <CodingEnthusiast@protonmail.com>
2022-07-01 12:33:37 +0430

    Add a default comparer

---
+++ Src/FinderOuter/Services/Comparers/DefaultComparer.cs
@@ -0,0 +1,24 @@
+﻿// The FinderOuter
+// Copyright (c) 2020 Coding Enthusiast
+// Distributed under the MIT software license, see the accompanying
+// file LICENCE or http://www.opensource.org/licenses/mit-license.php.
+
+using FinderOuter.Backend.ECC;
+
+namespace FinderOuter.Services.Comparers
+{
+    public class DefaultComparer : ICompareService
+    {
+        public bool Init(string data) => true;
+        public ICompareService Clone() => this;
+
+        private readonly Calc _calc = new();
+        public Calc Calc => _calc;
+
+        public unsafe bool Compare(uint* hPt) => true;
+        public unsafe bool Compare(ulong* hPt) => true;
+        public bool Compare(in PointJacobian point) => true;
+        public bool Compare(byte[] key) => true;
+        public bool Compare(Scalar key) => true;
+    }
+}
---
+++ Src/Tests/Services/Comparers/DefaultComparerTests.cs
@@ -0,0 +1,56 @@
+﻿// The FinderOuter
+// Copyright (c) 2020 Coding Enthusiast
+// Distributed under the MIT software license, see the accompanying
+// file LICENCE or http://www.opensource.org/licenses/mit-license.php.
+
+using FinderOuter.Backend.ECC;
+using FinderOuter.Services.Comparers;
+using Xunit;
+
+namespace Tests.Services.Comparers
+{
+    public class DefaultComparerTests
+    {
+        [Fact]
+        public void InitTest()
+        {
+            DefaultComparer comp = new();
+            Assert.True(comp.Init(""));
+        }
+
+        [Fact]
+        public void CloneTest()
+        {
+            DefaultComparer original = new();
+            ICompareService cloned = original.Clone();
+            Assert.Same(cloned, original);
+        }
+
+        [Fact]
+        public void CalcTest()
+        {
+            DefaultComparer comp = new();
+            Assert.NotNull(comp.Calc);
+        }
+
+        [Fact]
+        public void CompareTest()
+        {
+            DefaultComparer comp = new();
+            Assert.True(comp.Compare(new byte[1]));
+            Assert.True(comp.Compare(new PointJacobian()));
+            Assert.True(comp.Compare(new Scalar()));
+            unsafe
+            {
+                uint[] arr32 = new uint[1];
+                ulong[] arr64 = new ulong[1];
+                fixed (uint* h32 = &arr32[0])
+                fixed (ulong* h64 = &arr64[0])
+                {
+                    Assert.True(comp.Compare(h32));
+                    Assert.True(comp.Compare(h64));
+                }
+            }
+        }
+    }
+}
