fcd8064a20b9bb93e55f7e6bddbc2157dd959f70
Coding Enthusiast <CodingEnthusiast@protonmail.com>
2022-12-24 11:25:55 +0330

    Add a ViewModelBase test

---
+++ Src/Tests/ViewModels/ViewModelBaseTest.cs
@@ -0,0 +1,26 @@
+﻿// The FinderOuter
+// Copyright (c) 2020 Coding Enthusiast
+// Distributed under the MIT software license, see the accompanying
+// file LICENCE or http://www.opensource.org/licenses/mit-license.php.
+
+using FinderOuter.ViewModels;
+using Xunit;
+
+namespace Tests.ViewModels
+{
+    public class ViewModelBaseTest : ViewModelBase
+    {
+        [Fact]
+        public void RaiseCloseEventTest()
+        {
+            bool raised = false;
+            CLoseEvent += (sender, e) =>
+            {
+                raised = true;
+            };
+
+            RaiseCloseEvent();
+            Assert.True(raised);
+        }
+    }
+}
