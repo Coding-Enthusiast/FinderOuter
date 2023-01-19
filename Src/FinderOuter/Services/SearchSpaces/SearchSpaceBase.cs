9b5b531a391332e245fba70be86874523703809c
Coding Enthusiast <CodingEnthusiast@protonmail.com>
2023-01-19 18:51:21 +0330

    Add some process method to search-space base class

--- Src/FinderOuter/Services/SearchSpaces/SearchSpaceBase.cs
+++ Src/FinderOuter/Services/SearchSpaces/SearchSpaceBase.cs
@@ -3,7 +3,9 @@
 // Distributed under the MIT software license, see the accompanying
 // file LICENCE or http://www.opensource.org/licenses/mit-license.php.
 
+using FinderOuter.Backend;
 using System;
+using System.Diagnostics;
 using System.Numerics;
 
 namespace FinderOuter.Services.SearchSpaces
@@ -15,6 +17,94 @@ namespace FinderOuter.Services.SearchSpaces
         public int[] PermutationCounts { get; protected set; } = Array.Empty<int>();
         public int[] MissingIndexes { get; protected set; }
         public int MissCount { get; protected set; }
+
+
+        public bool ProcessValues(string[][] array, out string error)
+        {
+            if (array is null)
+            {
+                // ViewModels should never send null array
+                error = "Permutations list can not be null (this is a bug).";
+                return false;
+            }
+
+            if (array.Length != MissCount)
+            {
+                error = "Permutations list doesn't have the same number of arrays as missing characters count.";
+                return false;
+            }
+
+            int totalLen = 0;
+            int maxLen = 0;
+            int maxIndex = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] is null || array[i].Length < 2)
+                {
+                    error = $"Search space values are not correctly set. " +
+                            $"Add at least 2 possible values for the {(array.Length - i).ToOrdinal()} missing position.";
+                    return false;
+                }
+                totalLen += array[i].Length;
+
+                if (array[i].Length > maxLen)
+                {
+                    maxLen = array[i].Length;
+                    maxIndex = i;
+                }
+            }
+
+            if (maxIndex != 0)
+            {
+                string[] t1 = array[maxIndex];
+                array[maxIndex] = array[0];
+                array[0] = t1;
+
+                int t2 = MissingIndexes[maxIndex];
+                MissingIndexes[maxIndex] = MissingIndexes[0];
+                MissingIndexes[0] = t2;
+            }
+
+            AllPermutationValues = new uint[totalLen];
+            PermutationCounts = new int[MissCount];
+
+            error = string.Empty;
+            return true;
+        }
+
+
+        public bool ProcessCharValues(string[][] array, char[] allChars, out string error)
+        {
+            Debug.Assert(array is not null);
+            Debug.Assert(array.Length == MissCount);
+
+            int index1 = 0;
+            int index2 = 0;
+            foreach (string[] item in array)
+            {
+                Debug.Assert(item is not null && item.Length >= 2);
+
+                PermutationCounts[index2++] = item.Length;
+                foreach (string s in item)
+                {
+                    if (string.IsNullOrEmpty(s) || s.Length != 1)
+                    {
+                        error = $"Given value ({s}) is not a valid character.";
+                        return false;
+                    }
+                    int i = Array.IndexOf(allChars, s[0]);
+                    if (i < 0)
+                    {
+                        error = $"Given character ({s}) is not found in the valid characters list.";
+                        return false;
+                    }
+                    AllPermutationValues[index1++] = (uint)i;
+                }
+            }
+
+            error = string.Empty;
+            return true;
+        }
 
 
         public BigInteger GetTotal()
