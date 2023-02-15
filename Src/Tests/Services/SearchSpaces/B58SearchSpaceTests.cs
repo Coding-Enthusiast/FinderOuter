03a3d278e2d631d9a88ba7e6918f53b2980d31af
Coding Enthusiast <CodingEnthusiast@protonmail.com>
2023-02-15 06:51:12 +0330

    Add SetValues tests

--- Src/Tests/Services/SearchSpaces/B58SearchSpaceTests.cs
+++ Src/Tests/Services/SearchSpaces/B58SearchSpaceTests.cs
@@ -6,7 +6,9 @@
 using FinderOuter.Backend;
 using FinderOuter.Models;
 using FinderOuter.Services.SearchSpaces;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Numerics;
 using Xunit;
 
@@ -14,6 +16,15 @@ namespace Tests.Services.SearchSpaces
 {
     public class B58SearchSpaceTests
     {
+        private const string NoMiss = "L28Peud5cQcijrtMthAdUS8FynpM8PKZtnoUZb1VAio9WxKoebHt";
+        private const string OneMiss = "L28Peud5cQcijrtMthAdUS8FynpM8PKZtnoUZb1VAio9WxKoe*Ht";
+        private const string TwoMiss = "L28Pe*d5cQcij*tMthAdUS8FynpM8PKZtnoUZb1VAio9WxKoebHt";
+        private static readonly int[] OneMissIndex = new int[] { 49 };
+        private static readonly int[] OneMissIndexMult = new int[] { 20 };
+        private static readonly int[] TwoMissIndex = new int[] { 13, 5 };
+        private static readonly int[] TwoMissIndexMult = new int[] { 380, 460 };
+
+
         public static IEnumerable<object[]> GetShiftedMultCases()
         {
             for (int i = 0; i <= 24; i++)
@@ -265,6 +276,199 @@ namespace Tests.Services.SearchSpaces
             bool actual = ss.ProcessNoMissing(out string message);
             Assert.Equal(expected, actual);
             Assert.Contains(expMsg, message);
+        }
+
+        public static IEnumerable<object[]> GetSetValuesCases()
+        {
+            yield return new object[]
+            {
+                BuildSS(OneMiss, 1, Base58Type.PrivateKey, true),
+                null, null,
+                false, "Permutations list can not be null",
+                OneMissIndex, OneMissIndexMult,
+                null, Array.Empty<int>()
+            };
+            yield return new object[]
+            {
+                BuildSS(OneMiss, 1, Base58Type.PrivateKey, true),
+                Array.Empty<string[]>(), Array.Empty<string[]>(),
+                false, "Permutations list doesn't have the same number of arrays as missing characters count.",
+                OneMissIndex, OneMissIndexMult,
+                null, Array.Empty<int>()
+            };
+            yield return new object[]
+            {
+                BuildSS(OneMiss, 1, Base58Type.PrivateKey, true),
+                new string[2][] { new string[] { "" }, new string[] { "" } },
+                new string[2][] { new string[] { "" }, new string[] { "" } },
+                false,
+                "Permutations list doesn't have the same number of arrays as missing characters count.",
+                OneMissIndex, OneMissIndexMult, null, Array.Empty<int>()
+            };
+            yield return new object[]
+            {
+                BuildSS(TwoMiss, 2, Base58Type.PrivateKey, true),
+                new string[2][] { null, new string[2] { "a", "b" } },
+                new string[2][] { null, new string[2] { "a", "b" } },
+                false,
+                "Search space values are not correctly set. Add at least 2 possible values for the 1st missing position.",
+                TwoMissIndex, TwoMissIndexMult, null, Array.Empty<int>()
+            };
+            yield return new object[]
+            {
+                BuildSS(TwoMiss, 2, Base58Type.PrivateKey, true),
+                new string[2][] { new string[1] { "a" }, new string[2] { "a", "b" } },
+                new string[2][] { new string[1] { "a" }, new string[2] { "a", "b" } },
+                false,
+                "Search space values are not correctly set. Add at least 2 possible values for the 1st missing position.",
+                TwoMissIndex, TwoMissIndexMult, null, Array.Empty<int>()
+            };
+            yield return new object[]
+            {
+                BuildSS(TwoMiss, 2, Base58Type.PrivateKey, true),
+                new string[2][] { new string[2] { "a", "b" }, null },
+                new string[2][] { new string[2] { "a", "b" }, null },
+                false,
+                "Search space values are not correctly set. Add at least 2 possible values for the 2nd missing position.",
+                TwoMissIndex, TwoMissIndexMult, null, Array.Empty<int>()
+            };
+            yield return new object[]
+            {
+                BuildSS(TwoMiss, 2, Base58Type.PrivateKey, true),
+                new string[2][] { new string[2] { "a", "b" }, new string[1] { "a" } },
+                new string[2][] { new string[2] { "a", "b" }, new string[1] { "a" } },
+                false,
+                "Search space values are not correctly set. Add at least 2 possible values for the 2nd missing position.",
+                TwoMissIndex, TwoMissIndexMult, null, Array.Empty<int>()
+            };
+            yield return new object[]
+            {
+                BuildSS(TwoMiss, 2, Base58Type.PrivateKey, true),
+                new string[2][] { new string[2] { null, "b" }, new string[2] { "a", "b" } },
+                new string[2][] { new string[2] { null, "b" }, new string[2] { "a", "b" } },
+                false,
+                "Given value () is not a valid character.",
+                TwoMissIndex, TwoMissIndexMult, new uint[4], new int[2] {2, 0}
+            };
+            yield return new object[]
+            {
+                BuildSS(TwoMiss, 2, Base58Type.PrivateKey, true),
+                new string[2][] { new string[2] { "b", null }, new string[2] { "a", "b" } },
+                new string[2][] { new string[2] { "b", null }, new string[2] { "a", "b" } },
+                false,
+                "Given value () is not a valid character.",
+                TwoMissIndex, TwoMissIndexMult, new uint[4] {34,0, 0,0}, new int[2] {2,0}
+            };
+            yield return new object[]
+            {
+                BuildSS(TwoMiss, 2, Base58Type.PrivateKey, true),
+                new string[2][] { new string[2] { "1", "2" }, new string[2] { null, "b" } },
+                new string[2][] { new string[2] { "1", "2" }, new string[2] { null, "b" } },
+                false,
+                "Given value () is not a valid character.",
+                TwoMissIndex, TwoMissIndexMult, new uint[4] {0,1, 0,0}, new int[2] {2,2}
+            };
+            yield return new object[]
+            {
+                BuildSS(TwoMiss, 2, Base58Type.PrivateKey, true),
+                new string[2][] { new string[2] { "1", "2" }, new string[2] { "b", null } },
+                new string[2][] { new string[2] { "1", "2" }, new string[2] { "b", null } },
+                false,
+                "Given value () is not a valid character.",
+                TwoMissIndex, TwoMissIndexMult, new uint[4] {0,1, 34,0}, new int[2] {2,2}
+            };
+            yield return new object[]
+            {
+                BuildSS(TwoMiss, 2, Base58Type.PrivateKey, true),
+                new string[2][] { new string[2] { "11", "2" }, new string[2] { "b", "1" } },
+                new string[2][] { new string[2] { "11", "2" }, new string[2] { "b", "1" } },
+                false,
+                "Given value (11) is not a valid character.",
+                TwoMissIndex, TwoMissIndexMult, new uint[4] {0,0, 0,0}, new int[2] {2,0}
+            };
+            yield return new object[]
+            {
+                BuildSS(TwoMiss, 2, Base58Type.PrivateKey, true),
+                new string[2][] { new string[2] { "1", "2" }, new string[2] { "abc", "1" } },
+                new string[2][] { new string[2] { "1", "2" }, new string[2] { "abc", "1" } },
+                false,
+                "Given value (abc) is not a valid character.",
+                TwoMissIndex, TwoMissIndexMult, new uint[4] {0,1, 0,0}, new int[2] {2,2}
+            };
+            yield return new object[]
+            {
+                BuildSS(TwoMiss, 2, Base58Type.PrivateKey, true),
+                new string[2][] { new string[2] { "a", "O" }, new string[2] { "abc", "1" } },
+                new string[2][] { new string[2] { "a", "O" }, new string[2] { "abc", "1" } },
+                false,
+                "Given character (O) is not found in the valid characters list.",
+                TwoMissIndex, TwoMissIndexMult, new uint[4] {33,0, 0,0}, new int[2] {2,0}
+            };
+
+            // Valid lists:
+            yield return new object[]
+            {
+                BuildSS(TwoMiss, 2, Base58Type.PrivateKey, true),
+                new string[2][] { new string[2] { "5", "G" }, new string[2] { "h", "f" } },
+                new string[2][] { new string[2] { "5", "G" }, new string[2] { "h", "f" } },
+                true, string.Empty,
+                TwoMissIndex, TwoMissIndexMult, new uint[4] {4,15, 40,38}, new int[2] {2,2}
+            };
+            // The first array is bigger => no swap
+            yield return new object[]
+            {
+                BuildSS(TwoMiss, 2, Base58Type.PrivateKey, true),
+                new string[2][] { new string[3] { "5", "N", "a" }, new string[2] { "h", "f" } },
+                new string[2][] { new string[3] { "5", "N", "a" }, new string[2] { "h", "f" } },
+                true, string.Empty,
+                TwoMissIndex, TwoMissIndexMult, new uint[5] {4,21,33, 40,38}, new int[2] {3,2}
+            };
+            // The second array is bigger => swapped
+            yield return new object[]
+            {
+                BuildSS(TwoMiss, 2, Base58Type.PrivateKey, true),
+                new string[2][] { new string[2] { "5", "N" }, new string[4] { "9", "f", "r", "a" } },
+                new string[2][] { new string[4] { "9", "f", "r", "a" }, new string[2] { "5", "N" } },
+                true, string.Empty,
+                TwoMissIndex.Reverse().ToArray(), TwoMissIndexMult.Reverse().ToArray(),
+                new uint[6] {8,38,49,33, 4,21}, new int[2] {4,2}
+            };
+            yield return new object[]
+            {
+                BuildSS("5HueCGU8*MjxEXx*PuD5BDku4MkFqeZ*d4dZ1jvhTV*vbTLvyTJ", 4, Base58Type.PrivateKey, true),
+                new string[4][]
+                {
+                    new string[3] { "K", "u", "1" },
+                    new string[2] { "Y", "z" },
+                    new string[4] { "V", "p", "5", "a" },
+                    new string[3] { "M", "b", "4" }
+                },
+                new string[4][]
+                {
+                    new string[4] { "V", "p", "5", "a" },
+                    new string[2] { "Y", "z" },
+                    new string[3] { "K", "u", "1" },
+                    new string[3] { "M", "b", "4" }
+                },
+                true, string.Empty,
+                new int[4] {15,31,42,8},new int[4] {350,190,80,420},
+                new uint[12] {28,47,4,33, 31,57, 18,52,0, 20,34,3}, new int[4] {4,2,3,3}
+            };
+        }
+        [Theory]
+        [MemberData(nameof(GetSetValuesCases))]
+        public void SetValuesTest(B58SearchSpace ss, string[][] array, string[][] expArray, bool expected, string expMsg,
+                                  int[] expMissIndex, int[] expMissIndexMult, uint[] expPermVals, int[] expPermCounts)
+        {
+            bool actual = ss.SetValues(array, out string error);
+
+            Assert.Equal(expected, actual);
+            Assert.Contains(expMsg, error);
+            Assert.Equal(expArray, array);
+            Assert.Equal(expMissIndex, ss.MissingIndexes);
+            Assert.Equal(expMissIndexMult, ss.multMissingIndexes);
+            Assert.Equal(expPermVals, ss.AllPermutationValues);
+            Assert.Equal(expPermCounts, ss.PermutationCounts);
         }
     }
 }
