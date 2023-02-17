7ba9dd917517e19333a390123081c70ea8bd6fec
Coding Enthusiast <CodingEnthusiast@protonmail.com>
2023-02-17 12:57:07 +0330

    Add some converter tests

---
+++ Src/Tests/Backend/Mvvm/Converters/PossibilityToColorConverterTests.cs
@@ -0,0 +1,28 @@
+﻿// The FinderOuter
+// Copyright (c) 2020 Coding Enthusiast
+// Distributed under the MIT software license, see the accompanying
+// file LICENCE or http://www.opensource.org/licenses/mit-license.php.
+
+using Avalonia.Media;
+using FinderOuter.Backend.Mvvm.Converters;
+using FinderOuter.Models;
+using Xunit;
+
+namespace Tests.Backend.Mvvm.Converters
+{
+    public  class PossibilityToColorConverterTests
+    {
+        [Fact]
+        public void ConvertTest()
+        {
+            PossibilityToColorConverter conv = new();
+            object actual1  = conv.Convert(Possibility.Maybe, null, null, null);
+            object actual2  = conv.Convert(Possibility.Possible, null, null, null);
+            object actual3  = conv.Convert(Possibility.Impossible, null, null, null);
+
+            Assert.Equal(Brushes.Blue, actual1);
+            Assert.Equal(Brushes.Green, actual2);
+            Assert.Equal(Brushes.Red, actual3);
+        }
+    }
+}
---
+++ Src/Tests/Backend/Mvvm/Converters/PossibilityToStringConverterTests.cs
@@ -0,0 +1,25 @@
+﻿// The FinderOuter
+// Copyright (c) 2020 Coding Enthusiast
+// Distributed under the MIT software license, see the accompanying
+// file LICENCE or http://www.opensource.org/licenses/mit-license.php.
+
+using FinderOuter.Backend.Mvvm.Converters;
+using FinderOuter.Models;
+using Xunit;
+
+namespace Tests.Backend.Mvvm.Converters
+{
+    public class PossibilityToStringConverterTests
+    {
+        [Theory]
+        [InlineData(Possibility.Maybe, "?")]
+        [InlineData(Possibility.Possible, "✔")]
+        [InlineData(Possibility.Impossible, "X")]
+        public void ConvertTest(Possibility val, string expected)
+        {
+            PossibilityToStringConverter conv = new();
+            object actual = conv.Convert(val, null, null, null);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
