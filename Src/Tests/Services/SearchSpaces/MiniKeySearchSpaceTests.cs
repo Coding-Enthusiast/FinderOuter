ed611b4412e378b1dab2171250f4ab6aafcc15e1
Coding Enthusiast <CodingEnthusiast@protonmail.com>
2023-02-22 09:17:36 +0330

    Add SetValues tests

--- Src/Tests/Services/SearchSpaces/MiniKeySearchSpaceTests.cs
+++ Src/Tests/Services/SearchSpaces/MiniKeySearchSpaceTests.cs
@@ -14,6 +14,12 @@ namespace Tests.Services.SearchSpaces
 {
     public class MiniKeySearchSpaceTests
     {
+        private const string OneMiss = "SzavMBLoXU6kDrqtUVmff*";
+        private const string TwoMiss = "Sza*MBLoXU6*DrqtUVmffv";
+        private static readonly int[] OneMissIndex = new int[] { 21 };
+        private static readonly int[] TwoMissIndex = new int[] { 3, 11 };
+
+
         public static IEnumerable<object[]> GetProcessCases()
         {
             yield return new object[]
@@ -91,6 +97,71 @@ namespace Tests.Services.SearchSpaces
             b = ss.ProcessNoMissing(out msg);
             Assert.False(b);
             Assert.Contains("Invalid minikey.", msg);
+        }
+
+
+        private static MiniKeySearchSpace BuildSS(string s, int expMissCount, bool processResult)
+        {
+            MiniKeySearchSpace ss = new();
+            bool b = ss.Process(s, '*', out _);
+            Assert.Equal(expMissCount, ss.MissCount);
+            Assert.Equal(processResult, b);
+
+            return ss;
+        }
+
+        public static IEnumerable<object[]> GetSetValuesCases()
+        {
+            yield return new object[]
+            {
+                BuildSS(OneMiss, 1, true),
+                null, null,
+                false, "Permutations list can not be null",
+                OneMissIndex, null, Array.Empty<int>()
+            };
+            yield return new object[]
+            {
+                BuildSS(TwoMiss, 2, true),
+                new string[2][] { new string[2] { "1", "2" }, new string[2] { "b", null } },
+                new string[2][] { new string[2] { "1", "2" }, new string[2] { "b", null } },
+                false,
+                "Given value () is not a valid character.",
+                TwoMissIndex, new uint[4] {49,50, 98,0}, new int[2] {2,2}
+            };
+            yield return new object[]
+            {
+                BuildSS(TwoMiss, 2, true),
+                new string[2][] { new string[2] { "1", "0" }, new string[2] { "a", "1" } },
+                new string[2][] { new string[2] { "1", "0" }, new string[2] { "a", "1" } },
+                false,
+                "Given character (0) is not found in the valid characters list.",
+                TwoMissIndex, new uint[4] {49,0, 0,0}, new int[2] {2,0}
+            };
+
+            // Valid lists:
+            // The second array is bigger => swapped
+            yield return new object[]
+            {
+                BuildSS(TwoMiss, 2, true),
+                new string[2][] { new string[2] { "5", "B" }, new string[4] { "i", "D", "2", "x" } },
+                new string[2][] { new string[4] { "i", "D", "2", "x" }, new string[2] { "5", "B" } },
+                true, string.Empty,
+                TwoMissIndex.Reverse().ToArray(), new uint[6] {105,68,50,120, 53,66}, new int[2] {4,2}
+            };
+        }
+        [Theory]
+        [MemberData(nameof(GetSetValuesCases))]
+        public void SetValuesTest(MiniKeySearchSpace ss, string[][] array, string[][] expArray, bool expected, string expMsg,
+                                  int[] expMissIndex, uint[] expPermVals, int[] expPermCounts)
+        {
+            bool actual = ss.SetValues(array, out string error);
+
+            Assert.Equal(expected, actual);
+            Assert.Contains(expMsg, error);
+            Assert.Equal(expArray, array);
+            Assert.Equal(expMissIndex, ss.MissingIndexes);
+            Assert.Equal(expPermVals, ss.AllPermutationValues);
+            Assert.Equal(expPermCounts, ss.PermutationCounts);
         }
     }
 }
